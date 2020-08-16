bbfb3c0b34967a1f22e31df0eb07ae2f5f0fd964
Aseem Khullar <akhullar981@gmail.com>
2020-08-16 11:31:40 +0530

    Basic Backend

---
+++ app.js
@@ -0,0 +1,38 @@
+// For environment Variables
+require('dotenv').config()
+
+const express = require("express");
+const mongoose = require("mongoose")
+const app = express();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors")
+
+
+// Database Connection
+mongoose.connect(process.env.DATABASECLOUD, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => {
+  console.log("Database Connected")
+});
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//PORT
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send('Welcome CFG team-46')
+})
+
+
+app.listen(port, () => {
+  console.log('Server is up and running...')
+})
