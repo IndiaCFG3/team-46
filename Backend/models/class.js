baedfc6d73c1eb493844af2d8eb5d5ee2ea5eb23
Aseem Khullar <akhullar981@gmail.com>
2020-08-16 14:22:11 +0530

    Schema Added

---
+++ Backend/models/class.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var classSchema = new Schema({
+    studentteacherId: {
+        type: String,
+        maxlength: 16
+    },
+    classPresence: {
+        type: Number
+    },
+    initiative: {
+        type: Number
+    },
+    confidence: {
+        type: Number
+    },
+    preparation: {
+        type: Number
+    },
+    helpingTeams: {
+        type: Number
+    },
+    notesForMentor: {
+        type: String
+    }
+
+});
+
+module.exports = mongoose.model("classObservation",classSchema);
---
+++ Backend/models/unitEvaluation.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const {objectId} = mongoose.Schema;
+
+var unitEvaluationSchema = new Schema({
+   schoolName: {
+       type:String,
+       required: true
+   },
+   className: {
+        type: String,
+        required: true
+   },
+   unit: {
+       type: String,
+   },
+   teamNo: {
+       type: Number
+   },
+   ina: {
+    type: Number,
+    default: 0
+   },
+   ia: {
+    type: Number,
+    default: 0
+   },
+   ig: {
+    type: Number,
+    default: 0
+   },
+   is: {
+       type: Number,
+       default: 0
+   },
+   pna: {
+    type: Number,
+    default: 0
+   },
+   ppc: {
+    type: Number,
+    default: 0
+   },
+   pc: {
+    type: Number,
+    default: 0
+   },
+   mna: {
+       type: Number,
+       default: 0
+   },
+   mpc: {
+    type: Number,
+    default: 0
+   },
+   mc: {
+    type: Number,
+    default: 0
+   },
+   pna: {
+    type: Number,
+    default: 0
+   },
+   pa: {
+       type: Number,
+       default: 0
+   },
+   noOfStudents: {
+    type: Number,
+    default: 0
+   },
+   comment: {
+       type: String
+   },
+   st1: {
+    type: ObjectId,
+    ref: "Class"
+   },
+   st2: {
+    type: ObjectId,
+    ref: "Class"
+   }
+   
+});
+
+module.exports = mongoose.model("Unit",unitEvaluationSchema);
